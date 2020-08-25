8f687a9af3bf07eb7d4793f6009438cec0c06b43
= <immy01@hotmail.com>
2020-08-25 16:04:36 +0100

    Updates

---
+++ precache-manifest.570e091b3de9680241c78ed139784a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26b25ca8ee23a1b4cc48d3ccdf2a44c",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "c72713397ebcaaa90c6d",
+    "url": "/react-router/static/css/main.b4a8fe70.chunk.css"
+  },
+  {
+    "revision": "f31d392567e5ded5aab6",
+    "url": "/react-router/static/js/2.9eb10954.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router/static/js/2.9eb10954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72713397ebcaaa90c6d",
+    "url": "/react-router/static/js/main.a117baf8.chunk.js"
+  },
+  {
+    "revision": "db369e7f31bb10498cf3",
+    "url": "/react-router/static/js/runtime-main.048d0845.js"
+  }
+]);
